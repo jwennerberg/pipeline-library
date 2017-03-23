18c6a7f5df884029dfb8e198c18b5ff11e241d5e
Johan Wennerberg <jwennerberg@gmail.com>
2017-03-23 21:53:47 +0100

    read file

--- src/se/wnn/Utils.groovy
+++ src/se/wnn/Utils.groovy
@@ -13,8 +13,13 @@ def getNpmPackageVersion(){
   return pkg.version
 }
 
-def sonarQualityGateStatus(serverUrl, authString='foo'){
-  URLConnection projectStatus = new URL("${serverUrl}").openConnection();
+def sonarQualityGateStatus(propertiesFile, authString='foo'){
+  def file = readFile("${propertiesFile}");
+  Properties props = new Properties();
+  InputStream is = new ByteArrayInputStream(file.getBytes());
+  props.load(is);
+
+  URLConnection projectStatus = new URL(props.getProperty('serverUrl')).openConnection();
   projectStatus.setRequestProperty("Authorization", "Basic ${authString}");
 
   def status = new JsonSlurper().parse(new BufferedReader(new InputStreamReader(projectStatus.getInputStream())));
